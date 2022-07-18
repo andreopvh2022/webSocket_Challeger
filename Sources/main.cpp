393e4812955ece7e291e892c170671f2ae193333
André Luiz Alencar de Oliveira <andre.alencar@spread.com.br>
2022-07-18 11:37:57 -0300

    feat: Chalenger webSocket

---
+++ Sources/main.cpp
@@ -0,0 +1,14 @@
+// main.cpp
+
+#include <QCoreApplication>
+#include "myserver.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyServer server;
+    server.startServer();
+
+    return a.exec();
+}
